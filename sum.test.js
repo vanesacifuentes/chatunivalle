b4d9dbf698d0cc32786e1efe57919cdc8cd50270
fredyunivalle <fredy.ballesteros@correounivalle.edu.co>
2020-08-07 01:58:36 +0000

    arregla path file funciones

--- sum.test.js
+++ sum.test.js
@@ -1,4 +1,4 @@
-const sumaingresovalores = require('./Funcionesgestionsockets');
+const sumaingresovalores = require('./Pruebafunciones');
 
 test('probando la funcion suma 1 + 2 to equal 3', () => {
   expect(sumaingresovalores(1, 2)).toBe(3);
