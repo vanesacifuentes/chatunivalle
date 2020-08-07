1ff64a3d620ab63d62277887d777681f42f228a1
fredyunivalle <fredy.ballesteros@correounivalle.edu.co>
2020-08-07 01:11:09 +0000

    Mensaje y Zona Horaria

--- index.js
+++ index.js
@@ -27,7 +27,7 @@ io.on('connection', (socket) => {
     socket.join(user.room);
 
     // Welcome current user
-    socket.emit('message', formatMessage(botName, 'Bienvenidos al Chat'));
+    socket.emit('message', formatMessage(botName, 'Bienvenid al Chat'));
 
     // Broadcast when a user connects
     socket.broadcast
